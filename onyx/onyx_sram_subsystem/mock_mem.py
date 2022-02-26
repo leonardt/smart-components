64e1b041ffc936d009cdfe14de868c4b0ee23cf5
Caleb Donovick <donovick@cs.stanford.edu>
2022-02-25 20:59:25 -0800

    Add a stateful SRAM

--- onyx/onyx_sram_subsystem/mock_mem.py
+++ onyx/onyx_sram_subsystem/mock_mem.py
@@ -93,3 +93,53 @@ class SRAMDouble(SRAMBase):
         self.memory.RADDR @= self.io.WADDR
         self.memory.WADDR @= self.io.RADDR
 
+class SRAMStateful(SRAMDouble):
+    class CMD(m.Enum):
+        NOP  = 0  # have a nop so that CMD actually has more than 1 cmd
+        INIT = 1
+
+    class State(m.Enum):
+        SLEEP = 0
+        BOOT = 1
+        READY = 2
+
+    def __init__(self,
+            addr_width: int,
+            data_width: int,
+            has_byte_enable: bool,
+            ):
+
+        # set up some aliases
+        State = type(self).State
+        CMD = type(self).CMD
+        self.state = state = m.Register(init=State.SLEEP)()
+        self.current_state = current_state = state.O
+
+        super().__init__(addr_width, data_width, has_byte_enable)
+
+        self.io += m.IO(
+            cmd = m.In(CMD)
+        )
+
+        @m.inline_combinational()
+        def controller():
+            if self.io.CEn:
+                next_state = State.SLEEP
+            elif current_state == State.SLEEP:
+                next_state = State.BOOT
+            elif current_state == State.BOOT:
+                if self.io.cmd == CMD.INIT:
+                    # TODO
+                    # get the redudancy stuff
+                    next_state = State.READY
+                else:
+                    # cmd == CMD.NOP
+                    next_state = State.BOOT
+            else:
+                # current_state == READY
+                next_state = State.READY
+            state.I @= next_state
+
+    @property
+    def ce(self):
+        return super().ce & (self.current_state == type(self).State.READY)
--- onyx/tests/test_mock_mem.py
+++ onyx/tests/test_mock_mem.py
@@ -1,6 +1,8 @@
 from onyx_sram_subsystem.mock_mem import SRAMSingle
 from onyx_sram_subsystem.mock_mem import SRAMDouble
+from onyx_sram_subsystem.mock_mem import SRAMStateful
 
 def test_mock():
     single = SRAMSingle(32, 8, False)
     double = SRAMDouble(32, 8, False)
+    stateful = SRAMStateful(32, 8, False)
