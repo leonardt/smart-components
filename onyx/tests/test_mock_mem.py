218fa0c6670f3dd28f61d9f73600deb9b1d9df1e
Caleb Donovick <donovick@cs.stanford.edu>
2022-02-28 15:26:54 -0800

    Add real tests for single / double

--- onyx/tests/test_mock_mem.py
+++ onyx/tests/test_mock_mem.py
@@ -1,8 +1,60 @@
+import fault
+import hwtypes as hw
+import magma as m
+
 from onyx_sram_subsystem.mock_mem import SRAMSingle
 from onyx_sram_subsystem.mock_mem import SRAMDouble
 from onyx_sram_subsystem.mock_mem import SRAMStateful
 
-def test_mock():
-    single = SRAMSingle(32, 8, False)
-    double = SRAMDouble(32, 8, False)
-    stateful = SRAMStateful(32, 8, False)
+def test_single():
+    Single = SRAMSingle(4, 4, False)
+    tester = fault.Tester(Single, Single.CLK)
+
+    for i in range(1<<4):
+        tester.circuit.CEn = hw.Bit(0)
+        tester.circuit.REn = hw.Bit(0)
+        tester.circuit.WEn = hw.Bit(1)
+        tester.circuit.ADDR = hw.BitVector[4](i)
+        tester.circuit.WDATA = hw.BitVector[4](i)
+        tester.step(2)
+
+    for i in range(1<<4):
+        tester.circuit.CEn = hw.Bit(0)
+        tester.circuit.REn = hw.Bit(1)
+        tester.circuit.WEn = hw.Bit(0)
+        tester.circuit.ADDR = hw.BitVector[4](i)
+        tester.circuit.WDATA = hw.BitVector[4](0)
+        tester.step(2)
+        tester.circuit.RDATA.expect(hw.BitVector[4](i))
+    tester.compile_and_run("verilator", flags=["-Wno-fatal"])
+
+
+
+def test_double():
+    Double = SRAMDouble(4, 4, False)
+    tester = fault.Tester(Double, Double.CLK)
+
+    for i in range(1<<4):
+        tester.circuit.CEn = hw.Bit(0)
+        tester.circuit.REn = hw.Bit(0)
+        tester.circuit.WEn = hw.Bit(1)
+        tester.circuit.RADDR = hw.BitVector[4](0)
+        tester.circuit.WADDR = hw.BitVector[4](i)
+        tester.circuit.WDATA = hw.BitVector[4](i)
+        tester.step(2)
+
+    for i in range(1<<4):
+        tester.circuit.CEn = hw.Bit(0)
+        tester.circuit.REn = hw.Bit(1)
+        tester.circuit.WEn = hw.Bit(0)
+        tester.circuit.RADDR = hw.BitVector[4](i)
+        tester.circuit.WADDR = hw.BitVector[4](0)
+        tester.circuit.WDATA = hw.BitVector[4](0)
+        tester.step(2)
+        tester.circuit.RDATA.expect(hw.BitVector[4](i))
+
+    tester.compile_and_run("verilator", flags=["-Wno-fatal"])
+
+def test_stateful():
+    Stateful = SRAMStateful(4, 8, False, debug=True)
+
