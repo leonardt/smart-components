06808ea91e1f4007471b5bed5ad1a120e022a790
Caleb Donovick <donovick@cs.stanford.edu>
2022-06-06 14:37:08 -0700

    remove dead code

--- onyx/tests/test_mock_mem.py
+++ onyx/tests/test_mock_mem.py
@@ -7,7 +7,6 @@ import magma as m
 
 from onyx_sram_subsystem.mock_mem import SRAMSingle
 from onyx_sram_subsystem.mock_mem import SRAMDouble
-from onyx_sram_subsystem.mock_mem import SRAMStateful
 from onyx_sram_subsystem.mock_mem import SRAMRedundancyMixin
 from onyx_sram_subsystem.mock_mem import SRAMModalMixin
 from onyx_sram_subsystem.mock_mem import SRAM_FEATURE_TABLE
@@ -222,55 +221,5 @@ def test_redundancy(base, mixins, params):
             hw.BitVector[DATA_WIDTH](i) >> Definition.col_width *
             Definition.num_r_cols
         )
-
-    tester.compile_and_run("verilator", flags=["-Wno-fatal"])
-
-
-def test_stateful():
-    State = SRAMStateful.State
-    CMD = SRAMStateful.CMD
-    Stateful = SRAMStateful(ADDR_WIDTH, DATA_WIDTH, False, debug=True)
-    tester = fault.Tester(Stateful, Stateful.CLK)
-    for mask in map(hw.BitVector[DATA_WIDTH], (0, 1, 2)):
-        tester.circuit.CEn = hw.Bit(1)
-        tester.step(2)
-        tester.circuit.current_state.expect(State.SLEEP)
-
-        tester.circuit.CEn = hw.Bit(0)
-        tester.step(2)
-        tester.circuit.current_state.expect(State.BOOT)
-
-        tester.circuit.cmd = CMD.NOP
-        tester.step(2)
-        tester.circuit.current_state.expect(State.BOOT)
-        tester.circuit.cmd = CMD.INIT
-        tester.circuit.WDATA = mask
-        tester.step(2)
-        tester.circuit.current_state.expect(State.READY)
-        tester.circuit.mask.expect(mask[:2])
-
-        for i in range(1 << ADDR_WIDTH):
-            tester.circuit.CEn = hw.Bit(0)
-            tester.circuit.REn = hw.Bit(0)
-            tester.circuit.WEn = hw.Bit(1)
-            tester.circuit.RADDR = hw.BitVector[ADDR_WIDTH](0)
-            tester.circuit.WADDR = hw.BitVector[ADDR_WIDTH](i)
-            tester.circuit.WDATA = hw.BitVector[DATA_WIDTH](i)
-            tester.step(2)
-
-        for i in range(1 << ADDR_WIDTH):
-            tester.circuit.CEn = hw.Bit(0)
-            tester.circuit.REn = hw.Bit(1)
-            tester.circuit.WEn = hw.Bit(0)
-            tester.circuit.RADDR = hw.BitVector[ADDR_WIDTH](i)
-            tester.circuit.WADDR = hw.BitVector[ADDR_WIDTH](0)
-            tester.circuit.WDATA = hw.BitVector[DATA_WIDTH](0)
-            tester.step(2)
-            tester.circuit.RDATA.expect(hw.BitVector[DATA_WIDTH](i))
-            tester.step(2)
-
-        tester.circuit.CEn = hw.Bit(1)
-        tester.step(2)
-        tester.circuit.current_state.expect(State.SLEEP)
 
     tester.compile_and_run("verilator", flags=["-Wno-fatal"])
