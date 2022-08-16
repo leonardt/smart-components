3eba8e2f12861d90da408a8191c43ee284b3cebd
Caleb Donovick <donovick@cs.stanford.edu>
2022-08-16 10:14:38 -0700

    Small snippet

---
+++ onyx/onyx_sram_subsystem/state_machine.py
@@ -0,0 +1,18 @@
+import magma as m
+
+class CoopGenerator(m.Generator2):
+    def __init__(self, **kwargs):
+        self._decl_attrs(**kwargs)
+        self._decl_io(**kwargs)
+        self._decl_components(**kwargs)
+        self._connect(**kwargs)
+
+    def _decl_attrs(self, **kwargs): pass
+
+    def _decl_io(self, **kwargs):
+        self.io = m.IO()
+
+    def _decl_components(self, **kwargs): pass
+    def _connect(self, **kwargs): pass
+
+
