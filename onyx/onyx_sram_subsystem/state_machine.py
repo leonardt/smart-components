4aa406f8b14e27f41ba49080d38ecce3c64dc831
steveri <steveri@steveri.com>
2022-08-27 15:10:59 -0700

    implemented write-state actions

--- onyx/onyx_sram_subsystem/state_machine.py
+++ onyx/onyx_sram_subsystem/state_machine.py
@@ -25,6 +25,16 @@ class StateMachine(CoopGenerator):
         super()._decl_attrs(**kwargs)
         self.num_states = 4
 
+        # States
+        nbits = 2
+        self.MemInit = m.Bits[nbits](0)
+        self.MemOff  = m.Bits[nbits](1)
+        self.Send    = m.Bits[nbits](2)
+        self.MemOn   = m.Bits[nbits](3)
+
+
+
+
     def _decl_io(self, **kwargs):
         super()._decl_io(**kwargs)
         self.io += m.IO(
@@ -43,18 +53,21 @@ class StateMachine(CoopGenerator):
             has_enable=False,
         )()
 
+        # "send" reg
         # StateMachine.Register_inst1?
         self.s_reg = m.Register(
             T=m.Bits[16],
             has_enable=True,
         )()
 
+        # "receive" reg
         # StateMachine.Register_inst2?
         self.r_reg = m.Register(
             T=m.Bits[16],
             has_enable=True,
         )()
 
+        # "offer" (cmd) reg
         # StateMachine.Register_inst3?
         self.o_reg = m.Register(
             T=m.Bits[4],
@@ -74,183 +87,89 @@ class StateMachine(CoopGenerator):
         self.o_reg.I @= self.io.offer
         self.io.send @= self.s_reg.O
         
-
-
-
-
-##############################################################################
-        # FIXME added inits to avoid errors
-
-        # % python state_machine_debug.py |& grep driven
-        # ERROR:magma:StateMachine.Register_inst0.I not driven (state_reg?)
-        # ERROR:magma:StateMachine.Register_inst1.I not driven (s_reg?)
-        # ERROR:magma:StateMachine.Register_inst4.I not driven (redundancy_reg)
-        
-        # self.state_reg.I      @= m.Bits[2](0)  # StateMachine.Register_inst4
-        # self.s_reg.I          @= m.Bits[16](0) # StateMachine.Register_inst4
-        # self.redundancy_reg.I @= m.Bits[16](0) # StateMachine.Register_inst4
-##############################################################################
-
-
-
-
-
-        ################################################################
-        # This is the state machine we want to implement
-
-        # State MemInit
-        # Enable receive register 'r_reg'
-        # GOTO MemOff
-
-        # State MemOff
-        # Enable offer/cmd reg 'o_reg'
-        # If cmd == poweroff => GOTO MemOff
-        # if cmd == poweron  => GOTO Send
-
-        # State Send
-        # Enable send reg 's_reg'
-        # send wakeAckT
-        # GOTO MemOn
-
-        # State MemOn
-        # Enable offer/cmd reg 'o_reg'
-        # if cmd == poweroff => goto memoff
-        # if cmd == poweron => stay in state 2 (memon)
-        ################################################################
-
-        # States
-        MemInit = m.Bits[2](0)
-        MemOff  = m.Bits[2](1)
-        Send    = m.Bits[2](2)
-        MemOn   = m.Bits[2](3)
-
         # Commands
-        PowerOff = m.Bits[4](0)
-        PowerOn  = m.Bits[4](1)
+        nbits = 4   # Good for up to 16 commands
+        PowerOff = m.Bits[nbits](0)
+        PowerOn  = m.Bits[nbits](1) # not used
+        Read     = m.Bits[nbits](2)
+        Write    = m.Bits[nbits](3)
+        Idle     = m.Bits[nbits](4)
 
         # Convenient shortcuts
         cur_state = self.state_reg.O
         cmd       = self.o_reg.O
-        rcv       = self.r_reg.O
+        rcv_in    = self.r_reg.O
 
         # Enable registers only where needed (why? maybe not strictly necessary?)
         # TODO can try wiring all enables to 1'b1 and see if anything changes...?
 
         # MemInit uses r_reg, redundancy reg
-        self.r_reg.CE          @= (cur_state == MemInit)
-        self.redundancy_reg.CE @= (cur_state == MemInit)
+        self.r_reg.CE          @= (cur_state == self.MemInit)
+        self.redundancy_reg.CE @= (cur_state == self.MemInit)
 
         # Send uses s_reg
-        self.s_reg.CE          @= (cur_state == Send)
+        self.s_reg.CE          @= (cur_state == self.Send)
 
         # MemOff, MemOn use o_reg
-        self.o_reg.CE          @= ((cur_state == MemOff) | (cur_state == MemOn))
+        self.o_reg.CE          @= ((cur_state == self.MemOff) | (cur_state == self.MemOn))
 
-        # state update functions
-        # if state == MemInit: state = MemOff
-        # if cur_state == MemInit: cur_state = MemOff
-        # if cur_state == 0: cur_state = 1
+        # So, @mydecorator is just an easier way of saying myfunc = mydecorator(myfunc)
 
-        # So, @mydecorator is just an easier way of saying
-        # myfunc = mydecorator(myfunc)
-
-        # Note inline_combinational() is not very robust i.e.
-        # very particular about indentation even on comments, also
-        # should avoid e.g. one-line if-then ('if a: b=1'),
-        # multiple statements on one line separated by semicolon etc.
+        # ==============================================================================
+        # Note inline_combinational() is not very robust i.e. very particular
+        # about indentation even on comments, also should avoid e.g. one-line
+        # if-then ('if a: b=1'), multiple statements on one line separated by semicolon etc.
 
         @m.inline_combinational()
         def controller():
 
             # Dummy values for now
+            addr            = m.Bits[16](0)
             send_data       = m.Bits[16](0)
             redundancy_data = m.Bits[16](0)
             WakeAcktT       = m.Bits[16](1)
 
             # State MemInit
-            if cur_state == MemInit:
-                redundancy_data = rcv; next_state = MemOff
+            if cur_state == self.MemInit:
+                redundancy_data = self.r_reg.O    # Receive redundancy: output of receive reg => input of redundancy r
+                next_state = self.MemOff
 
             # State MemOff
-            elif ((cur_state == MemOff) & (cmd == PowerOff)):
-                next_state = MemOff
-            elif ((cur_state == MemOff) & (cmd == PowerOn )):
-                next_state = Send
+            elif ((cur_state == self.MemOff) & (cmd == PowerOff)):
+                next_state = self.MemOff
 
-            # State Send
-            elif cur_state == Send:
-                send_data  = WakeAcktT
-                next_state = MemOn
+            elif ((cur_state == self.MemOff) & (cmd == PowerOn )):
+                send_data  = WakeAcktT      # Send(WakeAck)
+                next_state = self.MemOn
 
             # State MemOn
-            elif ((cur_state == MemOn) & (cmd == PowerOff)):
-                next_state = MemOff
-            elif ((cur_state == MemOn) & (cmd == PowerOn )):
-                next_state = MemOn
+            elif ((cur_state == self.MemOn) & (cmd == PowerOff)):
+                next_state = self.MemOff
+
+            elif ((cur_state == self.MemOn) & (cmd == Idle )):
+                next_state = self.MemOn
+
+            # READ: get address from client, send back data from mem
+            elif ((cur_state == self.MemOn) & (cmd == Read )):
+                # TODO Receive(Addr)
+                # TODO Send(Data)
+                next_state = self.MemOn
+
+            # READ: get address and data from client, send to memory
+            # messages from client arrive via r_reg (receive reg)
+            elif ((cur_state == self.MemOn) & (cmd == Write )):
+                # TODO Receive(Addr)
+                addr = self.r_reg.O
+                # TODO Receive(Data)
+                data = self.r_reg.O
+                next_state = self.MemOn
+
+
 
             # Wire up our shortcuts
             self.state_reg.I      @= next_state
             self.s_reg.I          @= send_data
             self.redundancy_reg.I @= redundancy_data
-
-##############################################################################
-# UNUSED (for now)
-##############################################################################
-
-        # @m.inline_combinational()
-        def controller_alt():
-
-            def state_machine (smlist):
-                # assert n_elements%3 == 0?
-                while smlist:
-                    state1 = smlist.pop(0); # assert type int?
-                    action = smlist.pop(0); # assert type bool or func?
-                    state2 = smlist.pop(0); # assert type int?
-                    if cur_state == state1:
-                        # DO NOT eval action unless we are in appropriate state!
-                        if type(action) != bool: action = action()
-                        if action:
-                            return state2
-
-                # ERROR? ASSERT?
-                return state1
-
-            # Dummy values for now
-
-            send_data       = m.Bits[16](0)
-            redundancy_data = m.Bits[16](0)
-            WakeAcktT       = m.Bits[16](1)
-
-            def receive_redundancy():
-                redundancy_data = rcv 
-                return True
-
-            def send_wakeAckT():
-                send_data  = WakeAcktT
-                return True
-
-            next_state = state_machine([
-                MemInit, receive_redundancy,  MemOff,
-
-                MemOff, cmd == PowerOff,      MemOff,
-                MemOff, cmd == PowerOn,       Send  ,
-
-                Send,   cmd == send_wakeAckT, MemOn ,
-
-                MemOn,  cmd == PowerOff,      MemOff,
-                MemOn,  cmd == PowerOn,       MemOn ,
-            ])
-
-            # Wire up our shortcuts
-            self.state_reg.I      @= next_state
-            self.s_reg.I          @= send_data
-            self.redundancy_reg.I @= redundancy_data
-
-
-
-
-
-
 
 
 ##############################################################################
@@ -260,8 +179,8 @@ class StateMachine(CoopGenerator):
 
 def show_verilog():
     FSM = StateMachine()
-    m.compile("steveri/tmpdir/fsm0", FSM, output="coreir-verilog")
-    with open('steveri/tmpdir/fsm0.v', 'r') as f: print(f.read())
+    m.compile("steveri/tmpdir/fsm", FSM, output="coreir-verilog")
+    with open('steveri/tmpdir/fsm.v', 'r') as f: print(f.read())
 
 # show_verilog()
 print("==============================================================================")
