4e708e7275ee25887200e91b833afa5588895df6
steveri <steveri@steveri.com>
2022-11-04 16:06:42 -0700

    cleaning up

--- onyx/tests/test_mock_mem.py
+++ onyx/tests/test_mock_mem.py
@@ -15,116 +15,112 @@ ADDR_WIDTH = 8
 DATA_WIDTH = 8
 
 
-# # @pytest.mark.parametrize('base', [SRAMSingle, SRAMDouble])
-# @pytest.mark.parametrize('base', [SRAMSingle])
-# 
-# # Trailing commas in mixin parm tuples are *required* or it breaks...
-# @pytest.mark.parametrize(
-#     'mixins,                                  params',      [
-# #     ((),                                      {}),
-# #     ((SRAMModalMixin, ),                      {}),
-#     ((SRAMRedundancyMixin, ),                 { 'num_r_cols': 1 }),
-# #     ((SRAMRedundancyMixin, ),                 { 'num_r_cols': 2 }),
-# #     ((SRAMModalMixin, SRAMRedundancyMixin, ), { 'num_r_cols': 1 }),
-# #     ((SRAMModalMixin, SRAMRedundancyMixin, ), { 'num_r_cols': 2 }),
-#   ]
-# )
-# def test_sram(base, mixins, params):
-#     generator = SRAM_FEATURE_TABLE[base][frozenset(mixins)]
-#     Definition = generator(ADDR_WIDTH, DATA_WIDTH, debug=True, **params)
-#     tester = fault.Tester(Definition, Definition.CLK)
-# 
-#     if SRAMModalMixin in mixins:
-#         State = Definition.State
-# 
-#         # Stay asleep with power OFF => no wakeup
-#         tester.circuit.deep_sleep = hw.Bit(1)
-#         tester.circuit.power_gate = hw.Bit(1)
-#         tester.eval()
-#         tester.circuit.current_state.expect(State.DeepSleep)
-#         tester.circuit.wake_ack.expect(hw.Bit(0))
-# 
-#         # Wake up! But power still off => no wakeup
-#         tester.circuit.deep_sleep = hw.Bit(0)
-#         tester.circuit.power_gate = hw.Bit(1)
-#         tester.eval()
-#         tester.circuit.current_state.expect(State.TotalRetention)
-#         tester.circuit.wake_ack.expect(hw.Bit(0))
-# 
-#         # Go to sleep, but turn on power => no wakeup
-#         tester.circuit.deep_sleep = hw.Bit(1)
-#         tester.circuit.power_gate = hw.Bit(0)
-#         tester.eval()
-#         tester.circuit.current_state.expect(State.Retention)
-#         tester.circuit.wake_ack.expect(hw.Bit(0))
-# 
-#         # Wake up! Turn on power! => Finally we're awake maybe.
-#         tester.circuit.deep_sleep = hw.Bit(0)
-#         tester.circuit.power_gate = hw.Bit(0)
-#         tester.eval()
-#         tester.circuit.current_state.expect(State.Normal)
-#         tester.circuit.wake_ack.expect(hw.Bit(1))
-#         tester.step(2)
-#         tester.circuit.wake_ack.expect(hw.Bit(1))
-# 
-#     if SRAMRedundancyMixin in mixins:
-#         tester.circuit.RCE = hw.BitVector[params['num_r_cols']](-1)
-#         for i in range(params['num_r_cols']):
-#             setattr(
-#                 tester.circuit, f'RCF{i}A',
-#                 hw.BitVector[Definition.redundancy_addr_t.size](i)
-#             )
-# 
-#     # For i = 0 to MAX_ADDR, write i => SRAM[i]
-#     for i in range(1 << ADDR_WIDTH):
-#         tester.circuit.CEn = hw.Bit(0)
-#         tester.circuit.REn = hw.Bit(0)
-#         tester.circuit.WEn = hw.Bit(1)
-#         if base is SRAMSingle:
-#             tester.circuit.ADDR = hw.BitVector[ADDR_WIDTH](i)
-#         else:
-#             tester.circuit.RADDR = hw.BitVector[ADDR_WIDTH](0)
-#             tester.circuit.WADDR = hw.BitVector[ADDR_WIDTH](i)
-# 
-#         tester.circuit.WDATA = hw.BitVector[DATA_WIDTH](i)
-#         tester.step(2)
-# 
-#     # For i = 0 to MAX_ADDR, read SRAM[i] =? i
-#     for i in range(1 << ADDR_WIDTH):
-#         tester.circuit.CEn = hw.Bit(0)
-#         tester.circuit.REn = hw.Bit(1)
-#         tester.circuit.WEn = hw.Bit(0)
-#         if base is SRAMSingle:
-#             tester.circuit.ADDR = hw.BitVector[ADDR_WIDTH](i)
-#         else:
-#             tester.circuit.RADDR = hw.BitVector[ADDR_WIDTH](i)
-#             tester.circuit.WADDR = hw.BitVector[ADDR_WIDTH](0)
-#         tester.circuit.WDATA = hw.BitVector[DATA_WIDTH](0)
-#         tester.step(2)
-#         tester.circuit.RDATA.expect(hw.BitVector[DATA_WIDTH](i))
-# 
-#     tester.compile_and_run("verilator", flags=["-Wno-fatal"])
+@pytest.mark.parametrize('base', [SRAMSingle, SRAMDouble])
+
+# Trailing commas in mixin parm tuples are *required* or it breaks...
+@pytest.mark.parametrize(
+    'mixins,                                  params',      [
+    ((),                                      {}),
+    ((SRAMModalMixin, ),                      {}),
+    ((SRAMRedundancyMixin, ),                 { 'num_r_cols': 1 }),
+    ((SRAMRedundancyMixin, ),                 { 'num_r_cols': 2 }),
+    ((SRAMModalMixin, SRAMRedundancyMixin, ), { 'num_r_cols': 1 }),
+    ((SRAMModalMixin, SRAMRedundancyMixin, ), { 'num_r_cols': 2 }),
+  ]
+)
+def test_sram(base, mixins, params):
+    generator = SRAM_FEATURE_TABLE[base][frozenset(mixins)]
+    Definition = generator(ADDR_WIDTH, DATA_WIDTH, debug=True, **params)
+    tester = fault.Tester(Definition, Definition.CLK)
+
+    if SRAMModalMixin in mixins:
+        State = Definition.State
+
+        # Stay asleep with power OFF => no wakeup
+        tester.circuit.deep_sleep = hw.Bit(1)
+        tester.circuit.power_gate = hw.Bit(1)
+        tester.eval()
+        tester.circuit.current_state.expect(State.DeepSleep)
+        tester.circuit.wake_ack.expect(hw.Bit(0))
+
+        # Wake up! But power still off => no wakeup
+        tester.circuit.deep_sleep = hw.Bit(0)
+        tester.circuit.power_gate = hw.Bit(1)
+        tester.eval()
+        tester.circuit.current_state.expect(State.TotalRetention)
+        tester.circuit.wake_ack.expect(hw.Bit(0))
+
+        # Go to sleep, but turn on power => no wakeup
+        tester.circuit.deep_sleep = hw.Bit(1)
+        tester.circuit.power_gate = hw.Bit(0)
+        tester.eval()
+        tester.circuit.current_state.expect(State.Retention)
+        tester.circuit.wake_ack.expect(hw.Bit(0))
+
+        # Wake up! Turn on power! => Finally we're awake maybe.
+        tester.circuit.deep_sleep = hw.Bit(0)
+        tester.circuit.power_gate = hw.Bit(0)
+        tester.eval()
+        tester.circuit.current_state.expect(State.Normal)
+        tester.circuit.wake_ack.expect(hw.Bit(1))
+        tester.step(2)
+        tester.circuit.wake_ack.expect(hw.Bit(1))
+
+    if SRAMRedundancyMixin in mixins:
+        tester.circuit.RCE = hw.BitVector[params['num_r_cols']](-1)
+        for i in range(params['num_r_cols']):
+            setattr(
+                tester.circuit, f'RCF{i}A',
+                hw.BitVector[Definition.redundancy_addr_t.size](i)
+            )
+
+    # For i = 0 to MAX_ADDR, write i => SRAM[i]
+    for i in range(1 << ADDR_WIDTH):
+        tester.circuit.CEn = hw.Bit(0)
+        tester.circuit.REn = hw.Bit(0)
+        tester.circuit.WEn = hw.Bit(1)
+        if base is SRAMSingle:
+            tester.circuit.ADDR = hw.BitVector[ADDR_WIDTH](i)
+        else:
+            tester.circuit.RADDR = hw.BitVector[ADDR_WIDTH](0)
+            tester.circuit.WADDR = hw.BitVector[ADDR_WIDTH](i)
+
+        tester.circuit.WDATA = hw.BitVector[DATA_WIDTH](i)
+        tester.step(2)
+
+    # For i = 0 to MAX_ADDR, read SRAM[i] =? i
+    for i in range(1 << ADDR_WIDTH):
+        tester.circuit.CEn = hw.Bit(0)
+        tester.circuit.REn = hw.Bit(1)
+        tester.circuit.WEn = hw.Bit(0)
+        if base is SRAMSingle:
+            tester.circuit.ADDR = hw.BitVector[ADDR_WIDTH](i)
+        else:
+            tester.circuit.RADDR = hw.BitVector[ADDR_WIDTH](i)
+            tester.circuit.WADDR = hw.BitVector[ADDR_WIDTH](0)
+        tester.circuit.WDATA = hw.BitVector[DATA_WIDTH](0)
+        tester.step(2)
+        tester.circuit.RDATA.expect(hw.BitVector[DATA_WIDTH](i))
+
+    tester.compile_and_run("verilator", flags=["-Wno-fatal"])
 
 
-# @pytest.mark.parametrize('base', [SRAMSingle, SRAMDouble])
-@pytest.mark.parametrize('base', [SRAMSingle])
+@pytest.mark.parametrize('base', [SRAMSingle, SRAMDouble])
 @pytest.mark.parametrize(
     'mixins, params', [
         ((SRAMRedundancyMixin, ), {
             'num_r_cols': 1
-        }), 
-#         ((
-#             SRAMModalMixin,
-#             SRAMRedundancyMixin,
-#         ), {
-#             'num_r_cols': 1
-#         }), 
-#         ((
-#             SRAMModalMixin,
-#             SRAMRedundancyMixin,
-#         ), {
-#             'num_r_cols': 2
-#         })
+        }), ((
+            SRAMModalMixin,
+            SRAMRedundancyMixin,
+        ), {
+            'num_r_cols': 1
+        }), ((
+            SRAMModalMixin,
+            SRAMRedundancyMixin,
+        ), {
+            'num_r_cols': 2
+        })
     ]
 )
 def test_redundancy(base, mixins, params):
@@ -173,7 +169,6 @@ def test_redundancy(base, mixins, params):
             tester.circuit, f'RCF{i}A',
             hw.BitVector[m.bitutils.clog2safe(Definition.num_v_cols)](i)
         )
-    tester.circuit.RCF0A.expect(0)
 
     # Write 0 everywhere
     for i in range(1 << ADDR_WIDTH):
@@ -223,26 +218,9 @@ def test_redundancy(base, mixins, params):
             tester.circuit.WADDR = hw.BitVector[ADDR_WIDTH](0)
         tester.circuit.WDATA = hw.BitVector[DATA_WIDTH](0)
         tester.step(2)
-
         tester.circuit.RDATA.expect(
-            # hw.BitVector[DATA_WIDTH](i) >> Definition.col_width *
-            # Definition.num_r_cols
-
-            # hw.BitVector[DATA_WIDTH](i) >> 4
-
-            hw.BitVector[8](i) >> 4
-            # 0, 0, 0, ... 16(1), 16(2), ... 15, 15, 15
+            hw.BitVector[DATA_WIDTH](i) >> Definition.col_width *
+            Definition.num_r_cols
         )
-        if i == 15: tester.circuit.RDATA.expect(0)
-        if i == 16: tester.circuit.RDATA.expect(1)
-        if i == 32: tester.circuit.RDATA.expect(2)
-        if i == 255: tester.circuit.RDATA.expect(15)
-
-
-
-#         tester.circuit.RDATA.expect(
-#             4
-#         )
-# 
 
     tester.compile_and_run("verilator", flags=["-Wno-fatal"])
